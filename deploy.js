0c7963cbabb7c465cec6256d6c39af67924d0d4b
krishna bihari rajpt <erkbrajput@gmail.com>
2018-09-03 09:15:38 +0530

    deploy.js

---
+++ deploy.js
@@ -0,0 +1,21 @@
+const HDWalletProvider=require('truffle-hdwallet-provider');
+const Web3=require('web3');
+const { interface,bytecode}=require('./compile'); 
+
+const provider=new HDWalletProvider(
+     'purity pass chronic witness wink unique wish tornado mountain early wet lucky',
+     'https://rinkeby.infura.io/v3/7a187a9d753f4a3fbcd078ce81730ae3'
+	);
+const web3=new Web3(provider);
+const deploy= async () =>
+{
+const accounts=await web3.eth.getAccounts();
+console.log('Attempting to deploy from account',accounts[0]);
+
+const result= await new web3.eth.Contract(JSON.parse(interface))
+	.deploy({data:bytecode})
+	.send({from: accounts[0]});
+
+console.log('Contract deployed to',result.options.address);
+};
+deploy();
